df71bb084506add5eb1a86760b87d5a561ce5ae9
Madhav Menon <madhavanandmenon@gmail.com>
2026-07-13 11:40:22 +0530

    Finish SoaArrays implementation, finish custom CUDA error checker for memory allocation

--- src/soa.cpp
+++ src/soa.cpp
@@ -40,6 +40,28 @@ SoaArrays::~SoaArrays()
     capacity_ = 0;
 }
 
+void SoaArrays::check_cuda_memalloc_error(const cudaError_t cuda_error, const std::string& name) {
+    if (cuda_error != :cudaSuccess) {
+        throw std::runtime_error("Unsuccessful memory allocation for array: " + name);
+    }
+}
+
+void SoaArrays::reserve(const size_t message_count) {
+    if (order_ids_ != nullptr) { // Only need to check one array as all arrays are assigned together
+        throw std::runtime_error("reserve() has already been called before"); 
+    }
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&order_ids_), sizeof(std::uint64_t) * message_count), "order_ids_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&old_order_ids_), sizeof(std::uint64_t) * message_count), "old_order_ids_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&timestamps_), sizeof(std::uint64_t) * message_count), "timestamps_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&prices_), sizeof(std::uint32_t) * message_count), "prices_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&quantities_), sizeof(std::uint32_t) * message_count), "quantities_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&symbol_ids_), sizeof(std::uint16_t) * message_count), "symbol_ids_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&order_types_), sizeof(MessageType) * message_count), "order_types_");
+    check_cuda_memalloc_error(cudaMallocHost(reinterpret_cast<void**>(&sides_), sizeof(char) * message_count), "sides_");
+    size_ = 0;
+    capacity_ = message_count;
+}
+
 void SoaArrays::append(const DecodedOrder &decoded_order)
 {
     if (size_ >= capacity_)
