2bceaf00eeda107551c1054f0aca84c3f1c419af
Madhav Menon <madhavanandmenon@gmail.com>
2026-07-14 08:49:41 +0530

    Implement some hashing functionality

--- includes/hash_table.cuh
+++ includes/hash_table.cuh
@@ -3,6 +3,11 @@
 #include <cstddef>
 #include "cuda_utils.hpp"
 
+/* 
+ * Hash table kept on the GPU so that during reconstruction, no round trip has to be made back to the CPU
+ * If the hash table lives on the GPU, the latency involced in handing data to the CPU and back can be removed
+*/
+
 class GPUHashTable {
     public:
         GPUHashTable(size_t max_active_orders);
@@ -11,6 +16,12 @@ class GPUHashTable {
         GPUHashTable& operator=(const GPUHashTable&) = delete;
         GPUHashTable(GPUHashTable&&) = delete;
         GPUHashTable& operator=(GPUHashTable&&) = delete;
+        inline std::uint64_t* get_order_ids() const {return order_ids_;};
+        inline std::uint32_t* get_quantities() const {return quantities_;};
+        inline std::uint32_t* get_prices() const {return prices_;};
+        inline std::uint16_t* get_symbol_ids() const {return symbol_ids_;};
+        inline char* get_sides() const {return sides_;};
+        inline size_t capacity() const {return capacity_;};
     private:
         std::uint64_t* order_ids_; // key for the hash table, remaining member variables take SoA approach
         std::uint32_t* quantities_;
@@ -19,3 +30,8 @@ class GPUHashTable {
         char* sides_;
         size_t capacity_;  
 };
+
+__device__ std::uint64_t hash(std::uint64_t order_id);
+
+__global__ void insert(std::uint64_t order_id, std::uint32_t order_quantity, std::uint32_t order_price, std::uint16_t order_symbol_id, char order_side, std::uint64_t* order_ids, std::uint32_t* quantities, std::uint32_t* prices, std::uint16_t* symbol_ids, char* sides, size_t hash_table_capacity);
+
--- src/hash_table.cu
+++ src/hash_table.cu
@@ -29,3 +29,15 @@ GPUHashTable::~GPUHashTable() {
     }
     capacity_ = 0;
 }
+
+__device__ std::uint64_t hash(std::uint64_t order_id) {
+    // Used part of the splitmix64 algorithm as a hashing function
+    std::uint64_t hashed_order_id = (order_id ^ (order_id >> 30)) * 0xbf58476d1ce4e5b9;  /* xor the variable with the variable right bit shifted 30 then multiply by a constant */
+    hashed_order_id = (hashed_order_id ^ (hashed_order_id >> 27)) * 0x94d049bb133111eb;  /* xor the variable with the variable right bit shifted 27 then multiply by a constant */
+    return hashed_order_id ^ (hashed_order_id >> 31);                             /* return the variable xored with itself right bit shifted 31 */
+}
+
+__global__ void insert(std::uint64_t order_id, std::uint32_t order_quantity, std::uint32_t order_price, std::uint16_t order_symbol_id, char order_side, std::uint64_t* order_ids, std::uint32_t* quantities, std::uint32_t* prices, std::uint16_t* symbol_ids, char* sides, size_t hash_table_capacity) {
+    std::uint64_t hash_index = hash(order_id) % hash_table_capacity;
+    
+}
