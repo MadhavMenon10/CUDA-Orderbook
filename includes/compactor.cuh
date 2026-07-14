fd9a69b4f5287a278ccf699748b0f36ab35356ae
Madhav Menon <madhavanandmenon@gmail.com>
2026-07-14 18:58:11 +0530

    Write getters for SOA Arrays

---
+++ includes/compactor.cuh
@@ -0,0 +1,13 @@
+#pragma once
+
+class Compactor {
+    public:
+        Compactor();
+        ~Compactor();
+        Compactor(const Compactor&) = delete;
+        Compactor& operator=(const Compactor&) = delete;
+        Compactor(Compactor&&) = delete;
+        Compactore& operator=(Compactor&&) = delete;
+    private:
+    
+};
--- includes/soa.hpp
+++ includes/soa.hpp
@@ -6,22 +6,30 @@ class SoaArrays {
     public:
         SoaArrays();
         ~SoaArrays();
-        SoaArrays(const SoaArrays&) = delete;
-        SoaArrays& operator=(const SoaArrays&) = delete;
-        SoaArrays(SoaArrays&&) = delete;
-        SoaArrays& operator=(SoaArrays&&) = delete;
-        void append(const DecodedOrder& decoded_order);
+        SoaArrays(const SoaArrays &) = delete;
+        SoaArrays &operator=(const SoaArrays &) = delete;
+        SoaArrays(SoaArrays &&) = delete;
+        SoaArrays &operator=(SoaArrays &&) = delete;
+        void append(const DecodedOrder &decoded_order);
         void reserve(const size_t message_count);
+        inline std::uint64_t *get_order_ids() const { return order_ids_; };
+        inline std::uint64_t *get_old_order_ids() const { return old_order_ids_; };
+        inline std::uint64_t *get_timestamps() const { return timestamps_ };
+        inline std::uint32_t *get_quantities() const { return quantities_; };
+        inline std::uint32_t *get_prices() const { return prices_; };
+        inline std::uint16_t *get_symbol_ids() const { return symbol_ids_; };
+        inline char *get_sides() const { return sides_; };
+        inline size_t capacity() const { return capacity_; };
 
     private:
-        std::uint64_t* order_ids_;
-        std::uint64_t* old_order_ids_;
-        std::uint64_t* timestamps_;
-        std::uint32_t* prices_;
-        std::uint32_t* quantities_;
-        std::uint16_t* symbol_ids_;
-        MessageType* order_types_;
-        char* sides_;
+        std::uint64_t *order_ids_;
+        std::uint64_t *old_order_ids_;
+        std::uint64_t *timestamps_;
+        std::uint32_t *prices_;
+        std::uint32_t *quantities_;
+        std::uint16_t *symbol_ids_;
+        MessageType *order_types_;
+        char *sides_;
         size_t size_;
         size_t capacity_;
 };
