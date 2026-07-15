4742d360cb723560bd8b3c7e378f4481a39fbf00
Madhav Menon <madhavanandmenon@gmail.com>
2026-07-15 17:24:27 +0530

    Start barebones work on reconstructor

--- includes/reconstructor.cuh
+++ includes/reconstructor.cuh
@@ -1,1 +1,26 @@
 #pragma once
+#include <cstdint>
+
+struct PriceLevel {
+    std::uint32_t price;
+    std::uint32_t quantity;
+};
+
+struct OrderBookTick {
+    PriceLevel bids[5];
+    PriceLevel asks[5]; // Avoids std::array since these are consumed inside a CUDA kernel
+    std::uint64_t timestamp;
+};
+
+class OrderBookSnapshot {
+    public:
+        OrderBookSnapshot();
+        ~OrderBookSnapshot();
+        OrderBookSnapshot(const OrderBookSnapshot&) = delete;
+        OrderBookSnapshot& operator=(const OrderBookSnapshot&) = delete;
+        OrderBookSnapshot(OrderBookSnapshot&&) = delete;
+        OrderBookSnapshot& operator=(OrderBookSnapshot&&) = delete;
+    private:
+        
+
+};
